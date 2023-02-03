3a414b166f1f0c4b57f3ffa8709b264dfb9d8a3c
Moritz Mähr <moritz.maehr@gmail.com>
2023-02-03 16:38:33 +0100

    feat: added infoclio daisyui theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,22 @@ module.exports = {
 	theme: {
 		extend: {}
 	},
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#d11937',
+					secondary: '#afafaf',
+					accent: '#37CDBE',
+					neutral: '#010000',
+					'base-100': '#F0F0F0',
+					info: '#63c8c8',
+					success: '#95b96e',
+					warning: '#d7b95a',
+					error: '#dc878c'
+				}
+			}
+		]
+	},
 	plugins: [require('daisyui')]
 };
