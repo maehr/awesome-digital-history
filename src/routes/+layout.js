54f0c187657c115e28d48f65492b7fff9c825480
Moritz Mähr <moritz.maehr@gmail.com>
2023-03-06 00:12:02 +0100

    feat: footer added

---
+++ src/routes/+layout.js
@@ -0,0 +1,15 @@
+/** @type {import('./$types').LayoutLoad} */
+export async function load({ fetch }) {
+	const res = await fetch(
+		`https://api.github.com/repos/maehr/awesome-digital-history/commits/main`
+	);
+	const data = await res.json();
+	if (!res.ok) throw new Error(data.message);
+	const lastCommitDate = new Date(data.commit.author.date).toLocaleDateString();
+	const htmlURL = data.html_url;
+
+	return {
+		lastCommitDate,
+		htmlURL
+	};
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,28 @@
 <script>
 	import '../app.css';
 	import Header from './Header.svelte';
+	/** @type {import('./$types').LayoutData} */
+	export let data;
 </script>
 
 <div class="container mx-auto min-h-screen">
 	<Header />
 
 	<main>
-	<slot />
+		<slot />
 	</main>
+
+	<footer class="footer items-center p-4 text-white">
+		<div class="items-center grid-flow-col">
+			<p>
+				Licensed under CC0 and AGPLv3.
+			</p>
+			<p>
+				Last update <a href={data.htmlURL}
+					>{data.lastCommitDate}</a>.
+			</p>
+		</div>
+	</footer>
 </div>
 
 <style>
