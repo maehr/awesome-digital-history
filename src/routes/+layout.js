5c2feb937980ed4df8cce285d623c58dd346da8c
Moritz Mähr <moritz.maehr@gmail.com>
2023-03-06 10:46:48 +0100

    wip

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -4,12 +4,18 @@ export async function load({ fetch }) {
 		`https://api.github.com/repos/maehr/awesome-digital-history/commits/main`
 	);
 	const data = await res.json();
-	// if (!res.ok) throw new Error(data.message);
-	const lastCommitDate = new Date(data.commit.author.date).toLocaleDateString();
-	const htmlURL = data.html_url;
+	if (!res.ok) {
+		return {
+			lastCommitDate: 'unknown',
+			htmlURL: 'unknown'
+		};
+	} else {
+		const lastCommitDate = new Date(data.commit.author.date).toLocaleDateString();
+		const htmlURL = data.html_url;
 
-	return {
-		lastCommitDate,
-		htmlURL
-	};
+		return {
+			lastCommitDate,
+			htmlURL
+		};
+	}
 }
