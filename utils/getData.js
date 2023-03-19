1a6065e09e706297c50e39d1dd5cf31d4ea8c077
Moritz Mähr <moritz.maehr@gmail.com>
2023-03-19 16:16:02 +0100

    wip

--- utils/getData.js
+++ utils/getData.js
@@ -1,32 +1,30 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import fetch from 'node-fetch';
 
 async function getData(url, filepath) {
-	await fetch(url)
-		.then((response) => response.json())
-		.then((data) => {
-			fs.writeFile(filepath, JSON.stringify(data, null, 2), (err) => {
-				if (err) {
-					console.error(`Error writing file: ${err.message}`);
-					return;
-				}
-				console.log(`File "${filepath}" written successfully.`);
-			});
-		})
-		.catch((error) => console.error(error));
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		await fs.writeFile(filepath, JSON.stringify(data, null, 2));
+		console.log(`File "${filepath}" written successfully.`);
+	} catch (err) {
+		console.error(`Error writing file: ${err.message}`);
+	}
 }
 
 const data = [
 	{
-		url: `https://api.github.com/repos/maehr/awesome-digital-history/contributors`,
+		url: 'https://api.github.com/repos/maehr/awesome-digital-history/contributors',
 		filepath: 'src/lib/data/contributors.json'
 	},
 	{
-		url: `https://api.github.com/repos/maehr/awesome-digital-history/commits?per_page=1&status=success`,
+		url: 'https://api.github.com/repos/maehr/awesome-digital-history/commits?per_page=1&status=success',
 		filepath: 'src/lib/data/latest_commit.json'
 	}
 ];
 
-for (const item of data) {
-	getData(item.url, item.filepath);
-}
+(async function () {
+	for (const item of data) {
+		await getData(item.url, item.filepath);
+	}
+})();
