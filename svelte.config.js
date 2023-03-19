9c1cd27c2e4117343a571c591a8d2528ed9938df
Moritz Mähr <moritz.maehr@gmail.com>
2023-03-19 16:25:10 +0100

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import { vitePreprocess } from '@sveltejs/kit/vite';
@@ -9,9 +9,9 @@ const config = {
 	kit: {
 		adapter: adapter({ precompress: true }),
 		csp: { mode: 'auto' },
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/awesome-digital-history' : ''
-		}
+		// paths: {
+		// 	base: process.env.NODE_ENV === 'production' ? '/awesome-digital-history' : ''
+		// }
 	},
 	preprocess: [mdsvex(mdsvexConfig), vitePreprocess()]
 };
